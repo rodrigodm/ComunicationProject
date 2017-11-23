c117b330b94c45cde273b458b5adcd9b5845474a
Rodrigo Dorta <rodrigodorta@gmail.com>
2017-11-22 23:54:53 -0200

    New meshes save code
    new meshes new code (save)

---
+++ FPSNEW/Assets/Scripts(nonasset)/SaveMessage.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SaveMessage : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		Text.text;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
